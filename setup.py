840262e86fc1d0158fc3b14a55a0e6787f14b35b
Diana Spencer <diana.ponce.spencer@gmail.com>
2020-08-14 09:58:12 +0200

    init setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="spin-rl",
+    version="0.0.1",
+    author="Diana Spencer",
+    maintainer="Diana Spencer",
+    keywords="deep-reinforcement-learning rl deep-learning open-ai",
+    description="A gentle introduction to deep reinforcement learning with OpenAI",
+    packages=["spin-up-rl"],
+    license="MIT",
+    url="https://github.com/dianaspencer/spin-up-rl",
+    setup_requires=None,  # TODO
+    install_requires=None,  # TODO
+    test_require=None,  # TODO
+    test_suite=None,  # TODO
+)
